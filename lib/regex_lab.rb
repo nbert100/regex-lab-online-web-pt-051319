a784d07790ecbb890c3a824c2a5c0a4c1e2ab3cf
Nia Bert <nbert14@gmail.com>
2019-05-25 20:01:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.match(/\A[A-Z] && w\*
+if text.match(/\A[A-Z] && w\*\W\b
 end
 
 def valid_phone_number?(phone)
