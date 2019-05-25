18fee2db9ee1be006f14c8ad61f8a79ada923090
Nia Bert <nbert14@gmail.com>
2019-05-25 19:38:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,18 @@
-def starts_with_a_vowel?(word)
 
+def starts_with_a_vowel?(word)
+if word.match(/\A[aeiouAEIOU]/)
+  return true
+else
+  return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/length /)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
