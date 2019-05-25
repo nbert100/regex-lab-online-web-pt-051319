2c05459c968fe4f330a9f965f5e604d4c65766fc
Nia Bert <nbert14@gmail.com>
2019-05-25 20:13:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,5 +25,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-phone.match(/[0-9]{10}/)
+ if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+    return true
+  else
+    return false
+  end
 end
